2452c88f300fa110f4f7c011e4a6b1e0fe7e7dbd
wnagdidi <you@example.com>
2021-03-09 11:24:38 +0800

    first commit

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -2,6 +2,6 @@ class TestLogin:
     def test_login_001(self):
         assert 1
     def test_login_002(self):
-        assert 1
+        assert 0
     def test_login_003(self):
-        assert 1
+        assert 0
